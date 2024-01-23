877478c7338ad9688ef2d63eb470b24bc5972ba9
csewe56 <csewe56@gmail.com>
2024-01-23 00:06:44 -0800

    0x1B. C - Sorting algorithms & Big O

--- print_list.c
+++ print_list.c
@@ -1,19 +1,24 @@
-#include "sort.h"
 #include <stdio.h>
+#include "sort.h"
 
 /**
- * print_list - Prints a doubly linked list of integers
- * @list: Pointer to the head of the doubly linked list
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
  */
 void print_list(const listint_t *list)
 {
-	while (list != NULL)
-	{
-		printf("%d", list->n);
-		if (list->next != NULL)
-			printf(" <-> ");
-		list = list->next;
-	}
-	printf("\n");
+    int i;
+
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
+    }
+    printf("\n");
 }
 
